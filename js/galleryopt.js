31e6c3aec22d723d8a96e8bb980aac6fdd08cad8
Alex Kurchev <samson.alex97@gmail.com>
2017-02-22 01:57:13 +0200

    updated sources

---
+++ js/galleryopt.js
@@ -0,0 +1,12 @@
+  jQuery(document).ready(function(){
+
+                jQuery("#gallery").unitegallery({
+                gallery_theme: "tiles",
+                tiles_type: "justified",
+                gallery_width: "100%",
+                tiles_space_between_cols: 1,
+                tile_border_width: 0,
+                gallery_background_color: "#292b2c",
+                tiles_nested_optimal_tile_width: 263
+                });
+                });
---
+++ js/mapdata.js
@@ -0,0 +1,207 @@
+   function initMap() {
+        var myLatLng1 = {lat: 50.453314, lng: 30.542264};
+        var myLatLng2 = {lat: 47.838231, lng: 35.133075};        
+        var myLatLng3 = {lat: 47.572107, lng: 34.392514};
+        var myLatLng4 = {lat: 47.909953, lng: 33.391155};
+        var myLatLng5 = {lat: 46.975044, lng: 31.994758};
+        var myLatLng6 = {lat: 46.482206, lng: 30.719834};
+        var myLatLng7 = {lat: 44.956318, lng: 34.111364};
+        var myLatLng8 = {lat: 44.495713, lng: 34.166310};
+        var myLatLng9 = {lat: 45.191471, lng: 33.368752};
+        var myLatLng10 = {lat: 49.839311, lng: 24.025967};
+        var myLatLng11 = {lat: 48.923484, lng: 24.710261};
+        var myLatLng12 = {lat: 48.449323, lng: 22.708767};
+        var myLatLng13 = {lat: 48.669137, lng: 23.331805};
+        var myLatLng14 = {lat: 49.394578, lng: 22.467752};
+        var myLatLng15 = {lat: 49.554136, lng: 22.206109};        
+        var myLatLng16 = {lat: 49.695853, lng: 22.020196};
+        var myLatLng17 = {lat: 50.068223, lng: 19.937022};
+        var myLatLng18 = {lat: 52.229689, lng: 21.014568};
+        var myLatLng19 = {lat: 48.935702, lng: 21.919824};
+        var myLatLng20 = {lat: 49.268407, lng: 21.904673};
+        var myLatLng21 = {lat: 52.834299, lng: -118.044799};
+        var myLatLng22 = {lat: 53.034020, lng: -119.231335};
+        var myLatLng23 = {lat: 51.181845, lng: -115.574758};
+        var myLatLng24 = {lat: 53.404034, lng: -117.570235};
+        var myLatLng25 = {lat: 53.536814, lng: -113.501671};
+        var myLatLng26 = {lat: 52.269268, lng: -113.816340};
+        var myLatLng27 = {lat: 51.049373, lng: -114.072299};
+        var myLatLng = {lat: 41.354127, lng: -42.620151};
+          
+          
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: myLatLng,
+          mapTypeId: 'satellite'
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng1,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng2,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng3,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng4,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng5,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng6,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng7,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng8,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng9,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng10,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng11,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng12,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng13,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng14,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng15,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng16,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng17,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng18,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng19,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng20,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng21,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng22,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng23,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng24,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          var marker = new google.maps.Marker({
+          position: myLatLng25,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng26,
+          map: map,
+          title: 'Hello World!'
+        });
+        
+          var marker = new google.maps.Marker({
+          position: myLatLng27,
+          map: map,
+          title: 'Hello World!'
+        });
+          
+          
+
+      }
