e26c7a005a81300d36358fc67745148714a92e85
Alex Kurchev <samson.alex97@gmail.com>
2017-03-09 17:27:27 +0200

    new design

--- js/galleryopt.js
+++ js/galleryopt.js
@@ -2,11 +2,14 @@
 
                 jQuery("#gallery").unitegallery({
                 gallery_theme: "tiles",
-                tiles_type: "justified",
+                tiles_type: "coloumns",
                 gallery_width: "100%",
-                tiles_space_between_cols: 1,
-                tile_border_width: 0,
-                gallery_background_color: "#292b2c",
-                tiles_nested_optimal_tile_width: 263
+
+                gallery_background_color: "rgba(255, 255, 255, 0)",
+                tiles_set_initial_height: 263,
+    tiles_space_between_cols:25,
+	tiles_justified_space_between:25,
+	tiles_col_width:220,
+	tile_enable_shadow:true
                 });
                 });
